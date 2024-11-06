dc8312b4336d2fb609d6f1a356549634350014dd
arysitepu <arirzkita76@gmail.com>
2024-11-06 21:33:11 +0700

    belajar typescript

---
+++ bundle/index.js
@@ -0,0 +1,12 @@
+"use strict";
+let namaSaya = "Ary Sitepu";
+let username = 123;
+let pacarSaya;
+pacarSaya = ["ariel tatum", "sophialaa"];
+// tuple array
+let selingkuhanSaya;
+selingkuhanSaya = ["agnes", 30, true];
+console.log({ namaSaya });
+console.log({ username });
+console.log({ pacarSaya });
+console.log({ selingkuhanSaya });
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+let namaSaya: string = "Ary Sitepu";
+let username:number = 123;
+let pacarSaya:string[];
+pacarSaya = ["ariel tatum", "sophialaa"];
+
+// tuple array
+let selingkuhanSaya: [string, number, boolean];
+selingkuhanSaya = ["agnes", 30, true];
+console.log({namaSaya});
+console.log({username});
+console.log({pacarSaya});
+console.log({selingkuhanSaya});
